21555b76983c5333ce3002baf72562f663e3490f
ishaniIASC <31972289+ishaniIASC@users.noreply.github.com>
2017-12-05 17:05:37 -0500

    Add files via upload

---
+++ Exhibitions.js
@@ -0,0 +1,25 @@
+var countDownDate = new Date("Dec 29, 2017 15:37:25").getTime();
+
+var x = setInterval(function() {
+
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "Jan 2nd, 2018 from 8am to 11pm at Gagosian Gallery";
+  }
+}, 1000);
+
+//https://www.w3schools.com/howto/howto_js_countdown.asp
+//Advanced Site component + Javascript timer- countdown timer
